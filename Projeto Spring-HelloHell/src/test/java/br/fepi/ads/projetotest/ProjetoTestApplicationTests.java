ee12a6df075017aad6b41fcd773fafaabf23bfc6
Atarian <cgomespimentel7@gmail.com>
2023-02-23 14:14:05 -0300

    New Code

---
+++ Projeto Spring-HelloHell/src/test/java/br/fepi/ads/projetotest/ProjetoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fepi.ads.projetotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
